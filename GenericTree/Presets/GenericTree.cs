593c5898772891bffaa9d412fbeae19c5ef5bf28
Wasserwecken <eric.dolch@gmail.com>
2020-05-27 16:06:22 +0200

    add generic vector tree preset base

---
+++ GenericTree/Presets/GenericTree.cs
@@ -0,0 +1,16 @@
+﻿using GenericVector;
+
+namespace GenericTree.Presets
+{
+    public class GenericTree : Tree<Vector>
+    {
+        public GenericTree(Volume<Vector> startVolume, int maxDepth, int maxLeafsPerNode)
+            : base(startVolume, maxDepth, maxLeafsPerNode, VolumeSplit)
+        { }
+
+        private static Volume<Vector>[] VolumeSplit(Volume<Vector> volume)
+        {
+            return new Volume<Vector>[1];
+        }
+    }
+}
