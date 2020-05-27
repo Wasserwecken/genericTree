ad668d2d1fb0a88eb1c85c84b6dd6e01b2440e35
Wasserwecken <eric.dolch@gmail.com>
2020-05-27 20:00:04 +0200

    resturcture tests

--- Tests/FreeTreeTests.cs
+++ Tests/FreeTreeTests.cs
@@ -1,34 +1,165 @@
-using GenericVector;
+﻿using GenericTree;
 using GenericTree.Presets;
+using GenericVector;
 using NUnit.Framework;
+using static GenericTree.Presets.FreeTree.IntersectionType;
 
 namespace Tests
 {
     [TestFixture]
     public class FreeTreeTests
     {
-        public FreeTree tree;
-
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void PointBox()
         {
-            tree = new FreeTree(new Vector(0f, 0f), new Vector(4f, 4f), 4, 1);
+            var volume = new Volume<Vector>(new Vector(0f, 0f), new Vector(4f, 4f));
+
+            Assert.IsTrue(FreeTree.IntersectionTest.PointBox(new Vector(0f, 0f), volume));
+
+            Assert.IsTrue(FreeTree.IntersectionTest.PointBox(new Vector(1f, 1f), volume));
+            Assert.IsTrue(FreeTree.IntersectionTest.PointBox(new Vector(1f, -1f), volume));
+            Assert.IsTrue(FreeTree.IntersectionTest.PointBox(new Vector(-1f, 1f), volume));
+            Assert.IsTrue(FreeTree.IntersectionTest.PointBox(new Vector(-1f, -1f), volume));
+
+            Assert.IsTrue(FreeTree.IntersectionTest.PointBox(new Vector(1f, 0f), volume));
+            Assert.IsTrue(FreeTree.IntersectionTest.PointBox(new Vector(-1f, 0f), volume));
+            Assert.IsTrue(FreeTree.IntersectionTest.PointBox(new Vector(0f, 1f), volume));
+            Assert.IsTrue(FreeTree.IntersectionTest.PointBox(new Vector(0f, -1f), volume));
+
+            Assert.IsFalse(FreeTree.IntersectionTest.PointBox(new Vector(4f, 4f), volume));
+            Assert.IsFalse(FreeTree.IntersectionTest.PointBox(new Vector(4f, -4f), volume));
+            Assert.IsFalse(FreeTree.IntersectionTest.PointBox(new Vector(-4f, 4f), volume));
+            Assert.IsFalse(FreeTree.IntersectionTest.PointBox(new Vector(-4f, -4f), volume));
+
+            Assert.IsFalse(FreeTree.IntersectionTest.PointBox(new Vector(4f, 0f), volume));
+            Assert.IsFalse(FreeTree.IntersectionTest.PointBox(new Vector(-4f, 0f), volume));
+            Assert.IsFalse(FreeTree.IntersectionTest.PointBox(new Vector(0f, 4f), volume));
+            Assert.IsFalse(FreeTree.IntersectionTest.PointBox(new Vector(0f, -4f), volume));
         }
 
         [Test]
-        public void Add()
+        public void BoxBox()
         {
-            tree.Add(new FreeTreeLeafPoint(new Vector(1f, 1f)));
-            Assert.AreEqual(1, tree.LeafCount);
+            var volume = new Volume<Vector>(new Vector(0f, 0f), new Vector(2f, 2f));
+            Box box;
 
-            tree.Add(new FreeTreeLeafPoint(new Vector(1f, -1f)));
-            Assert.AreEqual(2, tree.LeafCount);
 
-            tree.Add(new FreeTreeLeafPoint(new Vector(-1f, 1f)));
-            Assert.AreEqual(3, tree.LeafCount);
+            box = new Box(new Vector(0f, 0f), new Vector(1f, 1f));
+            Assert.IsTrue(FreeTree.IntersectionTest.BoxBox(box, volume));
 
-            tree.Add(new FreeTreeLeafPoint(new Vector(-1f, -1f)));
-            Assert.AreEqual(4, tree.LeafCount);
+
+            box = new Box(new Vector(1f, 1f), new Vector(1f, 1f));
+            Assert.IsTrue(FreeTree.IntersectionTest.BoxBox(box, volume));
+
+            box = new Box(new Vector(1f, -1f), new Vector(1f, 1f));
+            Assert.IsTrue(FreeTree.IntersectionTest.BoxBox(box, volume));
+
+            box = new Box(new Vector(-1f, 1f), new Vector(1f, 1f));
+            Assert.IsTrue(FreeTree.IntersectionTest.BoxBox(box, volume));
+
+            box = new Box(new Vector(-1f, -1f), new Vector(1f, 1f));
+            Assert.IsTrue(FreeTree.IntersectionTest.BoxBox(box, volume));
+
+
+            box = new Box(new Vector(1f, 0f), new Vector(1f, 1f));
+            Assert.IsTrue(FreeTree.IntersectionTest.BoxBox(box, volume));
+
+            box = new Box(new Vector(-1f, 0f), new Vector(1f, 1f));
+            Assert.IsTrue(FreeTree.IntersectionTest.BoxBox(box, volume));
+
+            box = new Box(new Vector(0f, -1f), new Vector(1f, 1f));
+            Assert.IsTrue(FreeTree.IntersectionTest.BoxBox(box, volume));
+
+            box = new Box(new Vector(0f, 1f), new Vector(1f, 1f));
+            Assert.IsTrue(FreeTree.IntersectionTest.BoxBox(box, volume));
+
+
+            box = new Box(new Vector(2f, 2f), new Vector(1f, 1f));
+            Assert.IsFalse(FreeTree.IntersectionTest.BoxBox(box, volume));
+
+            box = new Box(new Vector(2f, -2f), new Vector(1f, 1f));
+            Assert.IsFalse(FreeTree.IntersectionTest.BoxBox(box, volume));
+
+            box = new Box(new Vector(-2f, 2f), new Vector(1f, 1f));
+            Assert.IsFalse(FreeTree.IntersectionTest.BoxBox(box, volume));
+
+            box = new Box(new Vector(-2f, -2f), new Vector(1f, 1f));
+            Assert.IsFalse(FreeTree.IntersectionTest.BoxBox(box, volume));
+
+
+            box = new Box(new Vector(2f, 0f), new Vector(1f, 1f));
+            Assert.IsFalse(FreeTree.IntersectionTest.BoxBox(box, volume));
+
+            box = new Box(new Vector(-2f, 0f), new Vector(1f, 1f));
+            Assert.IsFalse(FreeTree.IntersectionTest.BoxBox(box, volume));
+
+            box = new Box(new Vector(0f, 2f), new Vector(1f, 1f));
+            Assert.IsFalse(FreeTree.IntersectionTest.BoxBox(box, volume));
+
+            box = new Box(new Vector(0f, -2f), new Vector(1f, 1f));
+            Assert.IsFalse(FreeTree.IntersectionTest.BoxBox(box, volume));
+        }
+
+        [Test]
+        public void BoxSphere()
+        {
+            var volume = new Volume<Vector>(new Vector(0f, 0f), new Vector(1.5f, 1.5f));
+            Sphere sphere;
+
+            sphere = new Sphere(new Vector(0f, 0f), 1f);
+            Assert.IsTrue(FreeTree.IntersectionTest.SphereBox(sphere, volume));
+
+
+            sphere = new Sphere(new Vector(1f, 1f), 1f);
+            Assert.IsTrue(FreeTree.IntersectionTest.SphereBox(sphere, volume));
+
+            sphere = new Sphere(new Vector(1f, -1f), 1f);
+            Assert.IsTrue(FreeTree.IntersectionTest.SphereBox(sphere, volume));
+
+            sphere = new Sphere(new Vector(-1f, 1f), 1f);
+            Assert.IsTrue(FreeTree.IntersectionTest.SphereBox(sphere, volume));
+
+            sphere = new Sphere(new Vector(-1f, -1f), 1f);
+            Assert.IsTrue(FreeTree.IntersectionTest.SphereBox(sphere, volume));
+
+
+            sphere = new Sphere(new Vector(1f, 0f), 1f);
+            Assert.IsTrue(FreeTree.IntersectionTest.SphereBox(sphere, volume));
+
+            sphere = new Sphere(new Vector(-1f, 0f), 1f);
+            Assert.IsTrue(FreeTree.IntersectionTest.SphereBox(sphere, volume));
+
+            sphere = new Sphere(new Vector(0f, 1f), 1f);
+            Assert.IsTrue(FreeTree.IntersectionTest.SphereBox(sphere, volume));
+
+            sphere = new Sphere(new Vector(0f, -1f), 1f);
+            Assert.IsTrue(FreeTree.IntersectionTest.SphereBox(sphere, volume));
+
+
+            sphere = new Sphere(new Vector(2f, 2f), 1f);
+            Assert.IsFalse(FreeTree.IntersectionTest.SphereBox(sphere, volume));
+
+            sphere = new Sphere(new Vector(2f, -2f), 1f);
+            Assert.IsFalse(FreeTree.IntersectionTest.SphereBox(sphere, volume));
+
+            sphere = new Sphere(new Vector(-2f, 2f), 1f);
+            Assert.IsFalse(FreeTree.IntersectionTest.SphereBox(sphere, volume));
+
+            sphere = new Sphere(new Vector(-2f, -2f), 1f);
+            Assert.IsFalse(FreeTree.IntersectionTest.SphereBox(sphere, volume));
+
+
+            sphere = new Sphere(new Vector(2f, 0f), 1f);
+            Assert.IsFalse(FreeTree.IntersectionTest.SphereBox(sphere, volume));
+
+            sphere = new Sphere(new Vector(-2f, 0f), 1f);
+            Assert.IsFalse(FreeTree.IntersectionTest.SphereBox(sphere, volume));
+
+            sphere = new Sphere(new Vector(0f, 2f), 1f);
+            Assert.IsFalse(FreeTree.IntersectionTest.SphereBox(sphere, volume));
+
+            sphere = new Sphere(new Vector(0f, -2f), 1f);
+            Assert.IsFalse(FreeTree.IntersectionTest.SphereBox(sphere, volume));
         }
     }
 }
